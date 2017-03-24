8903ee8f079d58167530dd1bf61edb6f288e9563
Anton Westman <anton@fishbrain.com>
2017-03-24 20:11:35 +0100

    Send name in round serializer

--- app/serializers/game/round_serializer.rb
+++ app/serializers/game/round_serializer.rb
@@ -1,6 +1,6 @@
 module Game
   class RoundSerializer < ActiveModel::Serializer
-    attributes :id, :created_at, :alive_players_count, :dead_players_count, :ongoing
+    attributes :id, :created_at, :alive_players_count, :dead_players_count, :ongoing, :name
     has_many :players, serializer: PlayerSerializers::Base
 
     def alive_players_count
